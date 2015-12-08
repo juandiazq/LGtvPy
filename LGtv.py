0ce0236fc228c29b2b5ede32a4d3345223202d54
Juan Diaz <juandiazq@gmail.com>
2015-12-08 01:49:36 +0100

    modified:   LGtv.py 	modified:   lgtest.py

--- LGtv.py
+++ LGtv.py
@@ -50,6 +50,9 @@ class LGtv:
     def __init__(self, serialport, id=1):
         self.id = "%02X" % id
         self.ser = serial.Serial(serialport, 9600, timeout=1)
+    	""" Variable to store the current status """
+	self.currentVolume = -1
+	self.currentChannel = -1
 
     def __sendCmd(self, cmd):
         self.ser.flushInput()
@@ -108,6 +111,7 @@ class LGtv:
         """Set the volume, must be a level between 0 and 64"""
         if level > 64 or level < 0:
             raise ValueError
+    	self.currentVolume = level
         return self.__sendCmd('kf %s %02X\r' % (self.id, level))
 
     def setContrast(self, level):
@@ -196,4 +200,5 @@ class LGtv:
         """Set the channel"""
         if channelNo > 0x3E7 or channelNo <= 0:
             raise ValueError
+    	self.currentChannel = channelNo
     	return self.__sendCmd('ma %s %02X %02X %02X\r' % (self.id, ((channelNo & 0xff00) >> 8), (channelNo & 0x00ff), 0x10))
--- lgtest.py
+++ lgtest.py
@@ -3,8 +3,7 @@
 import LGtv
 
 tv = LGtv.LGtv('/dev/ttyUSB0')
-if tv.isOn() == False:
-	tv.powerOn()
-tv.setChannelDTV(9)
-
+print tv.isOn()
+if tv.isOn():
+	tv.powerOff()
 
